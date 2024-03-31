88e493598451f212b66d336bca5920d81bd30af2
Stark <karneatharv1920@gmail.com>
2024-03-31 11:40:22 +0530

    Refactoring done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const URL = require('./models/url');
 const app = express();
 const {connectToMongoDB,} = require('./connect');
 const urlRoute = require('./routes/url')
@@ -11,24 +10,9 @@ connectToMongoDB("mongodb://localhost:27017/short-url")
 app.use(express.json());
 
 app.use('/url' , urlRoute);
+app.use('/', urlRoute);
 
 
-app.get('/:shortId', async(req,res) => {
-    const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate(
-        {
-            shortId,
-        },
-        {
-            $push:
-            {
-                visitHistory: { timestamp:Date.now()},
-            },
-        }
-);
-
-res.redirect(entry.redirectURL);
-});
 
 app.listen(PORT , ()=>{
     console.log('Server running');
--- routes/url.js
+++ routes/url.js
@@ -1,10 +1,27 @@
 const express = require("express");
-
+const URL = require('../models/url');
 const { handleGenerateShortUrl } = require("../controllers/url")
 const router = express.Router();
 
 router.post("/" , handleGenerateShortUrl);
 
 
+
+router.get('/:shortId', async(req,res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId,
+        },
+        {
+            $push:
+            {
+                visitHistory: { timestamp:Date.now()},
+            },
+        }
+);
+
+res.redirect(entry.redirectURL);
+});
 
 module.exports = router;
