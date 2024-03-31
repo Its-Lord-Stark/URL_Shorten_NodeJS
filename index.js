4971db5ae60ddcf43edebd4ff57bfc2e02926ee0
Stark <karneatharv1920@gmail.com>
2024-03-31 11:34:01 +0530

    Url_Shorten working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const URL = require('./models/url');
 const app = express();
 const {connectToMongoDB,} = require('./connect');
 const urlRoute = require('./routes/url')
@@ -6,8 +7,29 @@ const PORT = 8010;
 
 connectToMongoDB("mongodb://localhost:27017/short-url")
 .then(() => {console.log('MongoDB connected')});
+
 app.use(express.json());
-app.use("/url" , urlRoute)
+
+app.use('/url' , urlRoute);
+
+
+app.get('/:shortId', async(req,res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId,
+        },
+        {
+            $push:
+            {
+                visitHistory: { timestamp:Date.now()},
+            },
+        }
+);
+
+res.redirect(entry.redirectURL);
+});
+
 app.listen(PORT , ()=>{
     console.log('Server running');
 })
--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,10 @@
 const express = require("express");
+
 const { handleGenerateShortUrl } = require("../controllers/url")
 const router = express.Router();
 
 router.post("/" , handleGenerateShortUrl);
+
+
 
 module.exports = router;
