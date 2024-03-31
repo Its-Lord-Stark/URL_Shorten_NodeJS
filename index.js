513c1d502d35582eb86eac3aef73ff67af45bf87
Stark <karneatharv1920@gmail.com>
2024-03-31 10:05:50 +0530

    DB connected

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const {connectToMongoDB,} = require('./connect');
+const urlRoute = require('./routes/url')
+const PORT = 8010;
+
+connectToMongoDB("mongodb://localhost:27017/short-url")
+.then(() => {console.log('MongoDB connected')});
+app.use(express.json());
+app.use("/url" , urlRoute)
+app.listen(PORT , ()=>{
+    console.log('Server running');
+})
+
